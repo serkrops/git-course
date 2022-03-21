f7e5426e3ab2d468152696067ee68736ffb709a7
serkrops <serg9205@gmail.com>
2022-03-21 11:18:41 +0200

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.body.innerHTML = "<h1>Hello from JavaScript</h1>"
 console.log("Привет мир!");
 console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
